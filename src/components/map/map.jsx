3f7a0a5c8fe85743f9998c44f317d56aca07c125
Revezer <64742445+Revezer@users.noreply.github.com>
2021-02-17 15:01:39 +0300

    карта

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -16,8 +16,6 @@ const Map = ({city, points}) => {
       zoom: city.zoom
     });
 
-    console.log(mapRef.current);
-
     leaflet
       .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
         attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
