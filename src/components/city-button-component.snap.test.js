6eab3da771ee1a5e7828522fa6e956c9265d9ef9
Revezer <64742445+Revezer@users.noreply.github.com>
2021-03-24 14:11:39 +0300

    tests

--- src/components/city-button-component.snap.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render} from '@testing-library/react';
-import {CityButton} from './city-button-component';
-
-it(`Should CityButton render correctly`, () => {
-  const {container} = render(<CityButton />);
-  expect(container).toMatchSnapshot();
-});
--- src/components/sort-offers-component.snap.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render} from '@testing-library/react';
-import {SortOffers} from './sort-offers-component';
-
-it(`Should SortOffers render correctly`, () => {
-  const {container} = render(<SortOffers />);
-  expect(container).toMatchSnapshot();
-});
